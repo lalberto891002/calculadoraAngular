765bf1dc1eaf74c8612185e6f9b5e2ae74d00994
lalberto891002 <luis.calvo@solinftec.com.br>
2020-10-23 15:20:22 -0300

    inyeccao do servico da calculadora no componente calculadora pronto

--- src/app/calculadora/componentes/calculadora.component.ts
+++ src/app/calculadora/componentes/calculadora.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CalculadoraService} from '../services';
+
 
 @Component({
   selector: 'app-calculadora',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calculadoraService:CalculadoraService) { 
+  	this.calculadoraService = calculadoraService;
+  }
 
   ngOnInit(): void {
   }
